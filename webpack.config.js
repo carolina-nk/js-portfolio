f77c30f1b76e8c766238a100b626e79e6894fd1d
caritonk <mcchavarriaga5@misena.edu.co>
2023-02-03 22:39:35 -0500

    //babel, loader de imagenes, stylus

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.js"
+        filename: "main.js",
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
         extensions: [".js"]
@@ -20,7 +24,34 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [MiniCSSExtractPlugin.loader,
+                    "css-loader",
+                    "stylus-loader"
+                ]
+            },
+            {
+                test: /\.png/,
+                type: "asset/resource"
             }
         ]
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCSSExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })
+    ]
 }
