48e4ad61a18e19406880c17153355f7b792743c4
caritonk <mcchavarriaga5@misena.edu.co>
2023-02-07 23:18:47 -0500

    //minificando hast css y js

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CSSMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        assetModuleFilename: 'assets/images/[hash][ext][query]'
+        //filename: "main.js",
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "assets/images/[hash][ext][query]"
     },
     resolve: {
         extensions: [".js"]
@@ -35,6 +38,20 @@ module.exports = {
             {
                 test: /\.png/,
                 type: "asset/resource"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: "url-loader",
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[contenthash].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule: false,
+                    }
+                }
             }
         ]
     },
@@ -44,7 +61,9 @@ module.exports = {
             template: "./public/index.html",
             filename: "./index.html"
         }),
-        new MiniCSSExtractPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: "assets/[name].[contenthash].css"
+        }),
         new CopyPlugin({
             patterns: [
                 {
@@ -53,5 +72,12 @@ module.exports = {
                 }
             ]
         })
-    ]
+    ],
+    optimization: {
+		minimize: true,
+		minimizer: [
+			new CSSMinimizerPlugin(),
+			new TerserPlugin()
+		]
+	}
 }
